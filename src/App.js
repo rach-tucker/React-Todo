b32f5a7a051e486f3c014e7b3719dd39803e1250
Rachel Tucker <rach.mello@hotmail.com>
2023-01-23 22:49:25 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import CreateACard from "./components/Card";
 
-function App() {
+
+function App(props) {
+
+  const l = [];
+  const [list, setList] = useState([]);
+  const [input, setInput] = useState("");
+
+  const addTodo = (todo) => {
+    l.push(todo);
+    const newTodo = {
+      id: Math.random(),
+      todo: todo
+    };
+
+  //add the todo to the list
+  setList([...list, newTodo]);
+
+  //clear input box
+  setInput("");
+};
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CreateACard list={l} />
+        <div>
+            <h1>To-do List</h1>
+            <input type="text"
+            value={input} 
+            onChange={(e) => setInput(e.target.value)}
+            />
+            <button onClick={() => addTodo(input)}>Add</button>
+        </div>
+        
+        <ul className = "card">
+            {list.map((todo) => (
+              <li key ={todo.id}>
+                {todo.todo}
+              </li>
+            ))}
+        </ul>
+    </>
   );
 }
 
